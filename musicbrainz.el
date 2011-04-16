73a10528415aa3d1b06f4a44537e6e6833060161
Lars Magne Ingebrigtsen <larsi@install.(none)>
2011-04-17 00:03:05 +0200

    Encode all strings in utf-8.

--- musicbrainz.el
+++ musicbrainz.el
@@ -108,15 +108,15 @@
 (defun musicbrainz-submit (toc cddb-entry)
   (let ((url (format
 	      "http://musicbrainz.org/ws/1/release/?client=musicbrainz.el-1.0&title=%s&toc=%s&discid=%s"
-	      (mm-url-form-encode-xwfu
+	      (musicbrainz-encode
 	       (cdr (assq 'title cddb-entry)))
-	      (mm-url-form-encode-xwfu
+	      (musicbrainz-encode
 	       (mapconcat #'number-to-string (cdr (assq 'toc toc)) " "))
 	      (cdr (assq 'id toc))))
 	(artist (cdr (assq 'artist cddb-entry)))
 	(i 0))
     (unless (equal artist "Various")
-      (setq url (concat url "&artist=" (mm-url-form-encode-xwfu artist))))
+      (setq url (concat url "&artist=" (musicbrainz-encode artist))))
     (dolist (track (cdr (assq 'tracks cddb-entry)))
       (if (equal artist "Various")
 	  (let ((elem (split-string track " - ")))
@@ -125,20 +125,23 @@
 		      (concat
 		       url (format "&track%d=%s&artist%d=%s"
 				   i
-				   (mm-url-form-encode-xwfu (cadr elem))
+				   (musicbrainz-encode (cadr elem))
 				   i
-				   (mm-url-form-encode-xwfu (car elem)))))
+				   (musicbrainz-encode (car elem)))))
 	      (setq url (concat url (format "&track%d=%s"
 					    i
-					    (mm-url-form-encode-xwfu track))))))
+					    (musicbrainz-encode track))))))
 	(setq url (concat url (format "&track%d=%s"
-				      i (mm-url-form-encode-xwfu track)))))
+				      i (musicbrainz-encode track)))))
       (incf i))
     (let* ((url-request-method "POST")
 	   (url-request-extra-headers '(("Content-length" . "0")))
 	   (buffer (url-retrieve-synchronously url)))
       (with-current-buffer buffer
 	(buffer-string)))))
+
+(defun musicbrainz-encode (string)
+  (mm-url-form-encode-xwfu (encode-coding-string string 'utf-8)))
 
 (provide 'musicbrainz)
 
